ff54732e364ac3abce186d8ebd28f06ac913681c
Robert Ispasoiu <ispasoiurobert@gmail.com>
2024-10-29 18:08:22 +0200

    My first commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+struct Doctor
+{
+    string d_name;
+    string d_speciality;
+};
+struct Pacient
+{
+    string p_name;
+    string p_speciality;
+};
+
+bool operator==(const Pacient& p1,const Doctor& d2) {
+    if (p1.p_speciality == d2.d_speciality)
+        return true;
+    else return false;
+}
 
 int main()
 {
@@ -10,6 +27,8 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
+    std::vector<Doctor> doctors;
+    std::vector<Pacient> pacients;
     
     inFile >> no_problems;
 
@@ -17,7 +36,13 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+
+        Pacient p;
+        p.p_name = name;
+        p.p_speciality = speciality;
+        pacients.emplace_back(p);
+
+        // cout << name << ' ' << speciality << '\n';
     }
 
     inFile >> no_doctors;
@@ -26,7 +51,20 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+
+        Doctor d;
+        d.d_name = name;
+        d.d_speciality = speciality;
+        doctors.emplace_back(d);
+
+        // cout << name << ' ' << speciality << '\n';
+    }
+
+    for (auto& pacient : pacients) {
+        auto it = find(doctors.begin(), doctors.end(), pacient);
+        if (it == doctors.end())
+            std::cout << pacient.p_name << ' ' << "Respins\n";
+        else std::cout << pacient.p_name << ' ' << "Acceptat\n";  
     }
 
     return 0;
