8e84ebe3e70b6ab785853558fa68d19f55bdc476
Robert Ispasoiu <ispasoiurobert@gmail.com>
2024-11-26 16:48:37 +0200

    solved

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <string>
 #include <vector>
-
 
 struct Doctor
 {
     std::string name;
     std::string speciality;
     bool available = true;  /// by default este true
+    int start = 9;
+    int finish = 17;
+    std::vector<std::string> problems;
 };
+
 struct Pacient
 {
     std::string name;
     std::string disease;
+    int time;
+    int priority;
 };
 
 bool operator==(const Pacient& p1,const Doctor& d2) {
@@ -28,6 +34,8 @@ int main()
     std::ifstream inFile("input3.txt");
 
     int no_problems, no_doctors;
+    int duration;
+    int _priority;
     std::string name, speciality;
     std::vector<Doctor> doctors;
     std::vector<Pacient> pacients;
@@ -37,10 +45,14 @@ int main()
     for (int i = 0; i < no_problems; i++) {
         inFile >> name;
         inFile >> speciality;
+        inFile >> duration;
+        inFile >> _priority;
 
         Pacient p;
         p.name = name;
         p.disease = speciality;
+        p.time = duration;
+        p.priority = _priority;
         pacients.emplace_back(p);
 
         // cout << name << ' ' << speciality << '\n';
@@ -60,20 +72,38 @@ int main()
         // cout << name << ' ' << speciality << '\n';
     }
 
-    for (auto& pacient : pacients) {
-        auto it = find_if(doctors.begin(), doctors.end(), [&pacient](const Doctor& doc) {
-            if (doc.speciality == pacient.disease and doc.available == true)
-                return true;
+    /// sortam vectorul de pacienti in functie de prioritate
+    sort(pacients.begin(), pacients.end(), [](const Pacient& pacient1, const Pacient& pacient2) {
+        return pacient1.priority > pacient2.priority;
+    });
+
+    for (const auto& pacient : pacients) {
+        auto it = find_if(doctors.begin(), doctors.end(), [&pacient](Doctor& doctor) {
+            if (pacient.disease == doctor.speciality) {
+                if (doctor.start + pacient.time <= 17) {
+                    doctor.start += pacient.time;
+                    return true;
+                }
+                else return false;  
+            }
             else return false;
         });
 
-        if (it != doctors.end()) {
-            (*it).available = false;    /// devine ocupat
-            std::cout << (*it).name << ' ' << (*it).speciality << '\n';
+        if (it != doctors.end()) {  /// daca a gasit in vectorul de doctori, pacientul respectiv
+            (*it).problems.emplace_back(pacient.name);   /// pune in vectorul de probleme rezolvate al doctorului respectiv, problema rezolvata
         }
     }
 
+    for (auto& doctor : doctors) {
+        if (doctor.problems.size()) {
+            std::cout << doctor.name << ' ' << doctor.problems.size() << ' ';
 
+            for (auto& problem : doctor.problems)
+                std::cout << problem << ' ';
+
+            std::cout << '\n';
+        }
+    }
 
     return 0;
 }
